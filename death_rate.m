09d961ac93b4e36e08c4b8c54c13c222764706b2
thelight0134 <amarhasan0134@gmail.com>
2021-01-14 20:39:06 +0800

    Saperate data analysis

--- death_rate.m
+++ death_rate.m
@@ -1,4 +1,6 @@
-function new_rate = infection_rate(current_rate)
-random_num = rand(1);
-new_rate = current_rate + (random_num - 0.5)*current_rate*10;
+function c = death_rate(e_death)
+% exponential distribution with mean = e_death
+
+c = -log(rand())*e_death;
+
 end
--- main.m
+++ main.m
@@ -10,32 +10,8 @@ year = 1;
 tmax = 52*year; % Number of weeks
 dt = 0.01; % Size of time step in weeks
 
-plotchoice = 20; 
+plotchoice = 5; 
 % 1=S | 2=I | 3=R | 4=D | 5=SIRD
-% 10=Data plot | 11=death_list
-% 20=List plot | 21= c_list | 22= b_list
-
-%% Data analysis
-data = readmatrix("covid_data.csv");
-[row,column] = size(data);
-maxWeek = row/7;
-
-infect_list = zeros(1,maxWeek);
-recover_list = zeros(1,maxWeek);
-death_list = zeros(1,maxWeek);
-
-a_list = zeros(1,maxWeek);
-b_list = zeros(1,maxWeek);
-c_list = zeros(1,maxWeek);
-
-for i = 1:maxWeek
-    infect_list(i) = sum(data(1+(i-1)*7:7*i , 2));
-    recover_list(i) = sum(data(1+(i-1)*7:7*i , 3));
-    death_list(i) = sum(data(1+(i-1)*7:7*i , 4));
-    
-    b_list(i) = recover_list(i) / data(i*7,5);
-    c_list(i) = death_list(i) / data(i*7,5);
-end
 
 %% Initialize Vectors
 t = 0:dt:tmax; % Time vector
@@ -49,10 +25,10 @@ I(1) = I0; % Set initial infection value
 
 %% Calculations
 for it = 1:Nt-1
-    a = infection_rate(a_list);
-    b = recovery_rate(b_list);
-    c = death_rate(c_list);
-    
+%     a = infection_rate(a_list);
+%     b = recovery_rate()
+%     c = death_rate(mean(c_list))
+%     
 %     S(it) = 1 - I(it) - R(it) - D(it); 
 %     
 %     dI = a*I(it)*S(it) - b*I(it) - c*I(it);
@@ -117,55 +93,4 @@ switch plotchoice
         title('Proportion of SIRD vs. Time')
         legend('S','I','R','D',...
                'Location','East')
-           
-    case 10
-        plot(1:maxWeek,infect_list,'-r',...
-             1:maxWeek,recover_list,'-b',...
-             1:maxWeek,death_list,'-m','LineWidth',2)
-        grid on
-        grid minor
-        xlabel('Weeks of data')
-        ylabel('Value of data')
-        title('Weekly value of IRD')
-        legend('Infected','Recovered','Death',...
-               'Location','East')
-           
-    case 11
-        plot(1:maxWeek, mean(death_list)*ones(maxWeek), '-r',...
-             1:maxWeek,death_list,'-b','LineWidth',2)
-        grid on
-        grid minor
-        xlabel('Weeks of data')
-        ylabel('Value of death')
-        title('Death value')
-        
-    case 20
-        plot(1:maxWeek,b_list,'-r',...
-             1:maxWeek,c_list,'-m','LineWidth',2)
-        grid on
-        grid minor
-        xlabel('Weeks of data')
-        ylabel('Value of list')
-        title('Weekly value of list')
-        legend('b-list','c-list',...
-               'Location','East')
-           
-    case 21
-        plot(1:maxWeek, mean(c_list)*ones(maxWeek), '-r',...
-             1:maxWeek,c_list,'-m','LineWidth',2)
-        grid on
-        grid minor
-        xlabel('Weeks of data')
-        ylabel('Value of c-list')
-        title('c-list')
-        
-    case 22
-        plot(1:maxWeek, mean(b_list)*ones(maxWeek), '-b',...
-             1:maxWeek,b_list,'-r','LineWidth',2)
-        grid on
-        grid minor
-        xlabel('Weeks of data')
-        ylabel('Value of b-list')
-        title('b-list')
-        
 end
